b96015a5a441c330d224c6fe11e59bfc5c70b486
Andreas Böttger <andreas.boettger@tuhh.de>
2019-03-07 18:32:12 +0100

    add implementation

--- src/gitlab_admin/api.py
+++ src/gitlab_admin/api.py
@@ -3,15 +3,19 @@
 from . helpers import __version__
 
 from . helpers.dua import Dua
+from . helpers.spl import Spl
 
 import argparse
 import gitlab
 
 
 def dua(args):
-
     dua = Dua(args)
     dua.main()
+
+def spl(args):
+    spl = Spl(args)
+    spl.main()
 
 
 def main():
@@ -24,10 +28,16 @@ def main():
                         help='Print the version of this program.', version='%(prog)s {}'.format(__version__))
 
     subparsers = parser.add_subparsers(help="Available subcommands")
+
     parser_dua = subparsers.add_parser('dua', help="Delete unconfirmed accounts")
     parser_dua.add_argument('--nono', action='store_true', help='Do not make any changes')
     parser_dua.add_argument('-t', '--timedelta', type=int, default=7, help='time delta')
     parser_dua.set_defaults(func=dua)
+
+    parser_spl = subparsers.add_parser('spl', help="Set the project limit of all accounts to a concrete value if the current value is smaller.")
+    parser_spl.add_argument('--nono', action='store_true', help='Do not make any changes')
+    parser_spl.add_argument('-l', '--limit', required=True, type=int, help='set project limit')
+    parser_spl.set_defaults(func=spl)
 
     args = parser.parse_args()
     args.func(args)
---
+++ src/gitlab_admin/helpers/spl.py
@@ -0,0 +1,44 @@
+import argparse
+import gitlab
+from datetime import datetime, timedelta
+
+
+class Spl:
+    def __init__(self, args):
+
+        self.gitlab_instance = args.gitlab_instance
+        self.private_token = args.private_token
+        self.nono = args.nono
+        self.limit = args.limit
+
+        try:
+            self.gl = gitlab.Gitlab(
+                self.gitlab_instance,
+                private_token=self.private_token)
+        except gitlab.config.GitlabConfigMissingError as err:
+            print(err)
+
+    def fetch_users(self):
+        users = self.gl.users.list(as_list=False)
+        return users
+
+    def fetch_user(self, id):
+        try:
+            user = self.gl.users.get(id)
+            return user
+        except gitlab.exceptions.GitlabGetError as err:
+            print(err)
+
+    def main(self):
+        if self.nono:
+            print('No changes will be made.')
+
+        for user in self.fetch_users():
+            if user.username != 'ghost':
+                if not user.external and self.limit > user.projects_limit:
+                    # print (user.external)
+                    # print (user.projects_limit)
+                    print('{} {:>5} {} {}'.format('set project limit to ' + str(self.limit) + ' (currently ' + str (user.projects_limit) + ')', user.id, user.username, user.email))
+                    if not self.nono:
+                       user.projects_limit = self.limit
+                       user.save()
