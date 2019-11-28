942c835f5774df9644ac698bbd0256ecde306d40
Andreas Böttger <andreas.boettger@tuhh.de>
2019-11-28 17:02:15 +0100

    improve scoring

--- src/gitlab_admin/helpers/bsa.py
+++ src/gitlab_admin/helpers/bsa.py
@@ -3,6 +3,7 @@ import json
 import signal
 import smtplib
 import sys
+from datetime import datetime, timedelta
 from pathlib import Path
 
 from gitlab import Gitlab, config, exceptions
@@ -26,6 +27,16 @@ class Bsa:
         self.nocache = nocache
         self.nono = nono
         self.cron = cron
+
+        self.trusted_domains = (
+            "tuhh.de", "tu-harburg.de", "uni-hamburg.de", "hcu-hamburg.de", "hsu-hh.de", "haw-hamburg.de",
+            "hfbk-hamburg.de")
+
+        self.trusted_countries = ("germany")
+
+        self.deadline_days = 10
+        self.deadline = datetime.today() - timedelta(self.deadline_days)
+
 
         self.whitelist_member_ids = set()
 
@@ -141,6 +152,47 @@ https://collaborating.tuhh.de/
             self.whitelist_member_ids.add(element.id)
         print()
 
+    def printFullInfo(self, element, projects_member_ids, groups_member_ids):
+        print("location\t" + str(element.location))
+        print("linkedin\t" + str(element.linkedin))
+        print("twitter\t" + str(element.twitter))
+        print("current_sign_in_at\t" + str(element.current_sign_in_at))
+        print("username\t" + str(element.username))
+        # print("highest_role\t" + str(element.highest_role))
+        print("avatar_url\t" + str(element.avatar_url))
+        print("projects_limit\t" + str(element.projects_limit))
+        print("last_sign_in_at\t" + str(element.last_sign_in_at))
+        print("name\t" + str(element.name))
+        print("bio\t" + str(element.bio))
+        print("last_activity_on\t" + str(element.last_activity_on))
+        print("id\t" + str(element.id))
+        print("identities\t" + str(element.identities))
+        print("color_scheme_id\t" + str(element.color_scheme_id))
+        print("web_url\t" + str(element.web_url))
+        print("website_url\t" + str(element.website_url))
+        print("email\t" + str(element.email))
+        print("confirmed_at\t" + str(element.confirmed_at))
+        print("external\t" + str(element.external))
+        print("theme_id\t" + str(element.theme_id))
+        print("private_profile\t" + str(element.private_profile))
+        print("is_admin\t" + str(element.is_admin))
+        print("skype\t" + str(element.skype))
+        print("organization\t" + str(element.organization))
+        print("created_at\t" + str(element.created_at))
+        print("can_create_group\t" + str(element.can_create_group))
+        print("two_factor_enabled\t" + str(element.two_factor_enabled))
+        print("public_email\t" + str(element.public_email))
+        print("can_create_project\t" + str(element.can_create_project))
+        print("state\t" + str(element.state))
+        if element.id in projects_member_ids:
+            print("project member\t" + "True")
+        else:
+            print("project member\t" + "False")
+        if element.id in groups_member_ids:
+            print("group member\t" + "True")
+        else:
+            print("group member\t" + "False")
+
     def main(self):
         if self.nono and not self.cron:
             print('No changes will be made.')
@@ -183,6 +235,7 @@ https://collaborating.tuhh.de/
             for project in projects:
                 try:
                     project_members = project.members.list(as_list=False)
+
                     for project_member in project_members:
                         projects_member_ids.add(project_member.id)
                 except Exception:
@@ -194,19 +247,133 @@ https://collaborating.tuhh.de/
             with open(self.path_whitelist, 'r') as handle:
                 self.whitelist_member_ids = set(json.loads(handle.read()))
 
+        score_defs = {}
+        score_defs["location"] = ({"yes": -10, "no": 0, "description": "Trusted counntry?"})
+        score_defs["projects_limit"] = ({"yes": -10, "no": 0, "description": "Project limit not 0?"})
+        score_defs["bio"] = ({"yes": -0, "no": 5, "description": "Unused bio?"})
+        score_defs["identities"] = ({"yes": -10, "no": 0, "description": "Additional identities?"})
+        score_defs["website_url"] = ({"yes": -0, "no": 5, "description": "Unused Website?"})
+        score_defs["email"] = ({"yes": -10, "no": 0, "description": "Trusted mail domain?"})
+        score_defs["private_profile"] = ({"yes": -10, "no": 0, "description": "Private profile?"})
+        score_defs["is_admin"] = ({"yes": -100, "no": 0, "description": "Is Admin?"})
+        score_defs["can_create_group"] = ({"yes": -10, "no": 0, "description": "Can create groups?"})
+        score_defs["can_create_project"] = ({"yes": -10, "no": 0, "description": "Can create projects?"})
+        score_defs["two_factor_enabled"] = ({"yes": -10, "no": 0, "description": "Two factor authentication enabled?"})
+        score_defs["projects_member_ids"] = ({"yes": -15, "no": 0, "description": "Is project member?"})
+        score_defs["groups_member_ids"] = ({"yes": -15, "no": 0, "description": "Is group member?"})
+        score_defs["last_activity_on"] = (
+            {"yes": -15, "no": 0, "description": "Acitivity in the last " + str(self.deadline_days) + " day(s) ago?"})
+
         for element in self.fetch_all():
-            if element.state == 'active' and element.id not in self.whitelist_member_ids and element.username != 'ghost' and element.external:
-                was_cached = False
+            if element.id == 3234:
+                score = 0
+                # if element.state == 'active' and element.username != 'ghost' and element.external:
+                # if element.state == 'active' and element.username != 'ghost':
+                if element.username != 'ghost':
+                    # self.printFullInfo(element, projects_member_ids, groups_member_ids)
+                    # score_def = "location"
+                    # yes_or_no = "yes" if  str(element.location).lower() in self.trusted_countries else "no"
+                    # print(element.location)
+                    # print(score_defs[score_def]["description"] + " " + yes_or_no + " -> " + str(
+                    #     score_defs[score_def][yes_or_no]))
+                    # score += score_defs[score_def][yes_or_no]
 
-                if not was_cached:
+                    score_def = "projects_limit"
+                    yes_or_no = "yes" if element.projects_limit > 0 else "no"
+                    print(score_defs[score_def]["description"] + " " + yes_or_no + " -> " + str(
+                        score_defs[score_def][yes_or_no]))
+                    score += score_defs[score_def][yes_or_no]
 
-                    if (
-                            element.website_url or element.bio) and element.id not in projects_member_ids and element.id not in groups_member_ids:
-                        print("Website oder Bio eingetragen und kein Mitglied in Gruppe und Projekt")
-                        print("website_url " + str(element.website_url))
-                        print("bio " + str(element.bio))
+                    score_def = "bio"
+                    yes_or_no = "yes" if str(element.bio) == 'None' or str(element.bio) == '' else "no"
+                    print(score_defs[score_def]["description"] + " " + yes_or_no + " -> " + str(
+                        score_defs[score_def][yes_or_no]))
+                    score += score_defs[score_def][yes_or_no]
+
+                    score_def = "identities"
+                    yes_or_no = "yes" if len(element.identities) > 0 else "no"
+                    print(score_defs[score_def]["description"] + " " + yes_or_no + " -> " + str(
+                        score_defs[score_def][yes_or_no]))
+                    score += score_defs[score_def][yes_or_no]
+
+                    score_def = "website_url"
+                    yes_or_no = "yes" if str(element.website_url) == 'None' or str(element.website_url) == '' else "no"
+                    print(score_defs[score_def]["description"] + " " + yes_or_no + " -> " + str(
+                        score_defs[score_def][yes_or_no]))
+                    score += score_defs[score_def][yes_or_no]
+
+                    score_def = "email"
+                    yes_or_no = "yes" if element.email.split('@')[1] in self.trusted_domains else "no"
+                    print(score_defs[score_def]["description"] + " " + yes_or_no + " -> " + str(
+                        score_defs[score_def][yes_or_no]))
+                    score += score_defs[score_def][yes_or_no]
+
+                    score_def = "private_profile"
+                    yes_or_no = "yes" if element.private_profile else "no"
+                    print(score_defs[score_def]["description"] + " " + yes_or_no + " -> " + str(
+                        score_defs[score_def][yes_or_no]))
+                    score += score_defs[score_def][yes_or_no]
+
+                    score_def = "is_admin"
+                    yes_or_no = "yes" if element.is_admin else "no"
+                    print(score_defs[score_def]["description"] + " " + yes_or_no + " -> " + str(
+                        score_defs[score_def][yes_or_no]))
+                    score += score_defs[score_def][yes_or_no]
+
+                    score_def = "can_create_group"
+                    yes_or_no = "yes" if element.can_create_group else "no"
+                    print(score_defs[score_def]["description"] + " " + yes_or_no + " -> " + str(
+                        score_defs[score_def][yes_or_no]))
+                    score += score_defs[score_def][yes_or_no]
+
+                    score_def = "two_factor_enabled"
+                    yes_or_no = "yes" if element.two_factor_enabled else "no"
+                    print(score_defs[score_def]["description"] + " " + yes_or_no + " -> " + str(
+                        score_defs[score_def][yes_or_no]))
+                    score += score_defs[score_def][yes_or_no]
+
+                    score_def = "can_create_project"
+                    yes_or_no = "yes" if element.can_create_project else "no"
+                    print(score_defs[score_def]["description"] + " " + yes_or_no + " -> " + str(
+                        score_defs[score_def][yes_or_no]))
+                    score += score_defs[score_def][yes_or_no]
+
+                    score_def = "projects_member_ids"
+                    yes_or_no = "yes" if element.id in projects_member_ids else "no"
+                    print(score_defs[score_def]["description"] + " " + yes_or_no + " -> " + str(
+                        score_defs[score_def][yes_or_no]))
+                    score += score_defs[score_def][yes_or_no]
+
+                    score_def = "groups_member_ids"
+                    yes_or_no = "yes" if element.id in groups_member_ids else "no"
+                    print(score_defs[score_def]["description"] + " " + yes_or_no + " -> " + str(
+                        score_defs[score_def][yes_or_no]))
+                    score += score_defs[score_def][yes_or_no]
+
+                    score_def = "last_activity_on"
+                    # print(datetime.strptime(element.last_activity_on, '%Y-%m-%d'))
+                    # print (self.deadline)
+                    yes_or_no = "yes" if element.last_activity_on and self.deadline < datetime.strptime(
+                        element.last_activity_on, '%Y-%m-%d') else "no"
+                    print(score_defs[score_def]["description"] + " " + yes_or_no + " -> " + str(
+                        score_defs[score_def][yes_or_no]))
+                    score += score_defs[score_def][yes_or_no]
+
+                    print("Score: " + str(score) + " (needed > 0 to classify as unused or spam)")
+
+                # if (element.website_url or element.bio) and element.id not in projects_member_ids and element.id not in groups_member_ids:
+                # was_cached = False
+                #
+                # if not was_cached:
+                #
+                #     if (
+                #             element.website_url or element.bio) and element.id not in projects_member_ids and element.id not in groups_member_ids:
+                #         print("Website oder Bio eingetragen und kein Mitglied in Gruppe und Projekt")
+                #         print("website_url " + str(element.website_url))
+                #         print("bio " + str(element.bio))
+                #         print("external" + str(element.external))
                         # Website oder Bio eingetragen und kein Mitglied in Gruppe und Projekt
-                        self.fire(element)
+                # self.fire(element)
                     # elif (element.website_url or element.bio) and (not re.match(r'.*\s.*', element.name) or element.name.islower()):
                     #     print("2")
                     #     print("website_url " + str(element.website_url))
@@ -214,5 +381,5 @@ https://collaborating.tuhh.de/
                     #     # Website oder Bio eingetragen und Name komplett klein und ohne Leerzeichen
                     #     self.fire(element)
 
-        with open(self.path_whitelist, 'w+') as handle:
-            json.dump(list(self.whitelist_member_ids), handle)
+        # with open(self.path_whitelist, 'w+') as handle:
+        #     json.dump(list(self.whitelist_member_ids), handle)
